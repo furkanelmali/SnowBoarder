b022ee4536b33b62311a80095d46e7741f5ed65e
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2023-07-09 19:27:18 +0300

    v1.2
    Control mechanics added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float torquePower = 1f;
   
     // Start is called before the first frame update
     void Start()
     {
-        
+      rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torquePower * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torquePower * Time.deltaTime);
+        }
     }
 }
